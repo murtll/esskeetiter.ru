7162ef278f8e399159bfb45e86a2c6ec402ca098
murtll <jkoz@inbox.ru>
2023-04-09 14:12:08 +0300

    add command outputs split file

---
+++ src/CommandOutputs.jsx
@@ -0,0 +1,37 @@
+export const files = ['about.txt', 
+                      'cv.md',
+                      'socials.md']
+
+export const longFiles = <p>
+        total 104
+        <br/>
+        -rw-r--r--   1 murtll murtll  451B Apr  8 16:46 about.txt
+        <br/>
+        -rw-r--r--   1 murtll murtll  301B Apr  8 17:41 cv.md
+        <br/>
+        -rw-r--r--   1 murtll murtll  163B Apr  8 16:42 socials.md
+    </p>
+
+export const help = <p>
+        whoami: shows current user
+        <br/>
+        exit: close terminal
+        <br/>
+        pwd: show current directory
+        <br/>
+        ls: list files in current directory
+        <br/>
+        cat: print contents of file
+        <br/>
+        help: display this help
+    </p>
+
+export const about = <p>
+    My name is <a href="https://github.com/murtll">@murtll</a> and currently I am working in <a href="https://onetwotrip.com">OneTwoTrip</a> as Junior DevOps Engineer.
+    <br/>
+    However, I am also interesed in many other IT areas, such as backend development and CLI/TUI utilities for Linux systems.
+    <br/>
+    Also I am working on some personal projects (such as <a href="https://mcbrawl.ru">mcbrawl.ru</a>), where I am trying to master some new for me technologies.
+    <br/>
+    Checkout my Github profile for some interesting projects.
+</p>
