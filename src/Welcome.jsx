59c355626cd940b379bc876de2701bf29c3da275
murtll <jkoz@inbox.ru>
2023-04-09 13:09:15 +0300

    add normal greeting

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,5 +1,18 @@
 export const Welcome = () => {
-    return <p>
-        Hello! I am <a className="glow" href="https://github.com/murtll">@murtll</a>
+    return <p className="glow">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_ &nbsp;&nbsp;_ _ 
+        <br/>
+        &nbsp;_ __ ___ &nbsp;_ &nbsp;&nbsp;_ _ __| |_| | |
+         <br/>
+        | '_ ` _ \| | | | '__| __| | |
+        <br/>
+        | | | | | | |_| | | &nbsp;| |_| | |
+        <br/>
+        |_| |_| |_|\__,_|_| &nbsp;&nbsp;\__|_|_|
+        <br/>
+        <br/>
+        Welcome to my personal website!
+        <br/>
+        Type help for the list of commands.
     </p>
 }
